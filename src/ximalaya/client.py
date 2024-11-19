3187fe9c710226ed022319d1581a6c051edf4fa9
Garrett HE <garrett.he@outlook.com>
2024-11-19 12:16:09 +0800

    Add class represents response paginators

--- src/ximalaya/client.py
+++ src/ximalaya/client.py
@@ -1,6 +1,9 @@
 import json
 import logging
 import urllib.request
+from typing import Generic, Iterator, TypeVar
+
+from jsonpath_ng import parse
 
 
 class XimalayaClient:
@@ -23,3 +26,32 @@ class XimalayaClient:
 
         with urllib.request.urlopen(req) as fp:
             return json.loads(fp.read().decode('utf-8'))
+
+
+T = TypeVar('T')
+
+
+class ResponsePaginator(Generic[T], Iterator):
+    client: XimalayaClient
+    url_path: str
+    page_name: str
+    page_num: int
+
+    def __init__(self, client: XimalayaClient, url_path: str, data_path: str, page_name: str = 'page', page_num: int = 1):
+        self.client = client
+        self.url_path = url_path
+        self.page_name = page_name
+        self.page_num = page_num
+        self.jsonpath_expression = parse(f'$.{data_path}')
+
+    def __next__(self) -> list[T]:
+        json_data = self.client.get(f'{self.url_path}&{self.page_name}={self.page_num}')
+
+        match = self.jsonpath_expression.find(json_data)
+
+        if match is None or len(match[0].value) == 0:
+            raise StopIteration()
+
+        self.page_num += 1
+
+        return match[0].value
